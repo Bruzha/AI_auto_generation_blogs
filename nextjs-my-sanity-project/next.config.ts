93505c2d756fe48d686ddb651fb18193b2d11486
Daria <dashabry15@gmail.com>
2025-08-13 12:06:37 +0300

    Update

--- nextjs-my-sanity-project/next.config.ts
+++ nextjs-my-sanity-project/next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+// const nextConfig: NextConfig = {
     
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    serverActions: true, // если используешь server actions
+  },
+  output: 'standalone', // Важно для Netlify и других хостингов
 };
 
 export default nextConfig;
